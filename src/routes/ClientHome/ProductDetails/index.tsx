1b7684acffaf648f79ed046517a349677acc023b
Gerson Klauck <gk120hf@gmail.com>
2025-12-09 18:32:18 -0300

    Links, catalog, product details

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -6,7 +6,7 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from '../../../services/product-service';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
   
@@ -23,7 +23,9 @@ export default function ProductDetails() {
           }          
           <div className="dsc-btn-page-container">
             <ButtonPrimary text="Comprar" />
-            <ButtonInverse text="Início" />
+            <Link to="/">
+              <ButtonInverse text="Início" />
+            </Link>
           </div>
         </section>
       </main>
