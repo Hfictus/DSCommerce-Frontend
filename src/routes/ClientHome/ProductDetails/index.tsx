52678df992b7a91223399e16a6e75c820672cacc
Gerson Klauck <gk120hf@gmail.com>
2025-12-11 11:41:50 -0300

    Request ProductById, Frontend -> Backend

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -5,19 +5,19 @@ import './styles.css';
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import type { ProductDTO } from '../../../models/product';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function ProductDetails() {
   
-  //const params = useParams();
+  const params = useParams();
 
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
-    axios.get("http://localhost:8080/products/1")
+    axios.get(`http://localhost:8080/products/${params.productId}`)
       .then(response => {
         console.log(response);
         setProduct(response.data);
