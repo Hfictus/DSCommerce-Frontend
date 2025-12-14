ff166997c988149a6bd69103605655f27f35a816
Gerson Klauck <gk120hf@gmail.com>
2025-12-14 11:33:07 -0300

    Getting an object from Local Storage using the get method

--- src/services/cart-service.ts
+++ src/services/cart-service.ts
@@ -8,3 +8,7 @@ import * as cartRepository from '../localstorage/cart-repository';
 export function saveCart(cart: OrderDTO) {
     cartRepository.save(cart);
 }
+
+export function getCart(): OrderDTO {
+    return cartRepository.get();
+}
