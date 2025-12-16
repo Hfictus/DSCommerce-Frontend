82e39642aaf707f3582370482f41413276306c33
Gerson Klauck <gk120hf@gmail.com>
2025-12-16 10:51:44 -0300

    Add link to Continuer comprando, button

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -6,6 +6,7 @@ import './styles.css';
 import { OrderDTO } from '../../../models/order';
 import * as cartService from '../../../services/cart-service';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
     
@@ -54,9 +55,11 @@ export default function Cart() {
                     <div className="dsc-btn dsc-btn-blue">
                         Finalizar pedido
                     </div>
-                    <div className="dsc-btn dsc-btn-white">
-                        Continuar comprando
-                    </div>
+                    <Link to="/catalog">
+                        <div className="dsc-btn dsc-btn-white">
+                            Continuar comprando
+                        </div>
+                    </Link>
                 </div>
             </section>
         </main>
