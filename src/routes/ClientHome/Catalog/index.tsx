421c0a1934ec90146bf2c050783455a12af92450
Gerson Klauck <gk120hf@gmail.com>
2025-12-18 09:19:28 -0300

    Catalog next page

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -15,6 +15,8 @@ type QueryParams = {
 
 export default function Catalog() {
   
+  const [isLastPage, setIsLastPage] = useState(false);
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
   
   const [queryParams, setQueryParams] = useState<QueryParams>({
@@ -25,12 +27,19 @@ export default function Catalog() {
   useEffect(() => {
     productService.findPageRequest(queryParams.page, queryParams.name)
       .then(response => {
-          setProducts(response.data.content);
+        const nextPage = response.data.content;  
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
         });
   }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setQueryParams({...queryParams, name: searchText });
+    setProducts([]);
+    setQueryParams({...queryParams, page: 0, name: searchText });
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1 });
   }
 
   return(
@@ -43,9 +52,13 @@ export default function Catalog() {
                   product => <CatalogCard key={product.id} product={product} />
                 )
               }
-              
             </div>
-            <ButtonNextPage />
+            {
+              !isLastPage &&
+              <div onClick={handleNextPageClick}>
+                <ButtonNextPage />
+              </div>
+            }
         </section>
       </main>
     );
