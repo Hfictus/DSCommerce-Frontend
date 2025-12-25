d034603c06e0c949874917e611a9e2f7d1c18f73
Gerson Klauck <gk120hf@gmail.com>
2025-12-25 17:23:13 -0300

    Redirecting outside React component

--- .pnp.cjs
+++ .pnp.cjs
@@ -40,6 +40,7 @@ const RAW_RUNTIME_STATE =
           ["eslint-plugin-react-hooks", "virtual:ca1f9da14e12defe8f532bc9a403cbacfa4cdda2f2bea487c1848c2deb6284b86ed7dc1c6542f47134ceae179ab5262b1875c463ed75c32c3cce02d44d28dd6a#npm:7.0.1"],\
           ["eslint-plugin-react-refresh", "virtual:ca1f9da14e12defe8f532bc9a403cbacfa4cdda2f2bea487c1848c2deb6284b86ed7dc1c6542f47134ceae179ab5262b1875c463ed75c32c3cce02d44d28dd6a#npm:0.4.24"],\
           ["globals", "npm:16.5.0"],\
+          ["history", "npm:5.3.0"],\
           ["qs", "npm:6.11.0"],\
           ["react", "npm:19.2.0"],\
           ["react-dom", "virtual:ca1f9da14e12defe8f532bc9a403cbacfa4cdda2f2bea487c1848c2deb6284b86ed7dc1c6542f47134ceae179ab5262b1875c463ed75c32c3cce02d44d28dd6a#npm:19.2.0"],\
@@ -268,6 +269,15 @@ const RAW_RUNTIME_STATE =
         "packagePeers": [\
           "@babel/core",\
           "@types/babel__core"\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["@babel/runtime", [\
+      ["npm:7.28.4", {\
+        "packageLocation": "../../Users/Gerson/AppData/Local/Yarn/Berry/cache/@babel-runtime-npm-7.28.4-31bc1b0001-10c0.zip/node_modules/@babel/runtime/",\
+        "packageDependencies": [\
+          ["@babel/runtime", "npm:7.28.4"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -1779,6 +1789,7 @@ const RAW_RUNTIME_STATE =
           ["eslint-plugin-react-hooks", "virtual:ca1f9da14e12defe8f532bc9a403cbacfa4cdda2f2bea487c1848c2deb6284b86ed7dc1c6542f47134ceae179ab5262b1875c463ed75c32c3cce02d44d28dd6a#npm:7.0.1"],\
           ["eslint-plugin-react-refresh", "virtual:ca1f9da14e12defe8f532bc9a403cbacfa4cdda2f2bea487c1848c2deb6284b86ed7dc1c6542f47134ceae179ab5262b1875c463ed75c32c3cce02d44d28dd6a#npm:0.4.24"],\
           ["globals", "npm:16.5.0"],\
+          ["history", "npm:5.3.0"],\
           ["qs", "npm:6.11.0"],\
           ["react", "npm:19.2.0"],\
           ["react-dom", "virtual:ca1f9da14e12defe8f532bc9a403cbacfa4cdda2f2bea487c1848c2deb6284b86ed7dc1c6542f47134ceae179ab5262b1875c463ed75c32c3cce02d44d28dd6a#npm:19.2.0"],\
@@ -2448,6 +2459,16 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["hermes-estree", "npm:0.25.1"],\
           ["hermes-parser", "npm:0.25.1"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["history", [\
+      ["npm:5.3.0", {\
+        "packageLocation": "../../Users/Gerson/AppData/Local/Yarn/Berry/cache/history-npm-5.3.0-00136b6a63-10c0.zip/node_modules/history/",\
+        "packageDependencies": [\
+          ["@babel/runtime", "npm:7.28.4"],\
+          ["history", "npm:5.3.0"]\
         ],\
         "linkType": "HARD"\
       }]\
--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -5,6 +5,7 @@ import type { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
 import axios from "axios";
 import * as authService from '../services/auth-service';
+import { history } from './history';
 
 export function requestBackend(config: AxiosRequestConfig) {
     
@@ -17,3 +18,33 @@ export function requestBackend(config: AxiosRequestConfig) {
 
     return axios({...config, baseURL: BASE_URL, headers});
 }
+
+//REQUEST INTERCEPTOR
+axios.interceptors.request.use(
+    function (config) {
+        //DO SOMETHING BEFORE REQUEST IS SENT
+        return config;
+    },
+    function (error) {
+        //DO SOMETHING WITH REQUEST ERROR
+        return Promise.reject(error);
+    }
+);
+
+//RESPONSE INTERCEPTOR
+axios.interceptors.response.use(
+    function (response) {
+        //DO SOMETHING WITH RESPONSE DATA IF STATUS IS 2xx
+        return response;
+    },
+    function (error) {
+        if(error.response.status === 401) {
+            history.push("/login");
+        }
+        if(error.response.status === 403) {
+            history.push("/catalog");
+        }
+        //DO SOMETHING WITH RESPONSE ERROR
+        return Promise.reject(error);
+    }
+);
