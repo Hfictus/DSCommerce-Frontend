80658d75cc3a5cf665bd379a8777dfb6f91a3cc3
Gerson Klauck <gk120hf@gmail.com>
2025-12-28 17:03:54 -0300

    LoggedUser

--- src/components/HeaderAdmin/index.tsx
+++ src/components/HeaderAdmin/index.tsx
@@ -2,6 +2,7 @@
 import './styles.css';
 import homeIcon from '../../assets/home.svg';
 import productsIcon from '../../assets/products.svg';
+import LoggedUser from '../LoggedUser';
 
 export default function HeaderAdmin() {
     return(
@@ -10,19 +11,16 @@ export default function HeaderAdmin() {
                 <h1>DSC Admin</h1>
                 <div className="dsc-navbar-right">
                     <div className="dsc-menu-items-container">
-            <div className="dsc-menu-item">
-              <img src={homeIcon} alt="Início" />
-              <p>Início</p>
-            </div>
-            <div className="dsc-menu-item">
-              <img src={productsIcon} alt="Cadastro de produtos" />
-              <p className="dsc-menu-item-active">Produtos</p>
-            </div>
+                        <div className="dsc-menu-item">
+                            <img src={homeIcon} alt="Início" />
+                            <p>Início</p>
+                        </div>
+                        <div className="dsc-menu-item">
+                            <img src={productsIcon} alt="Cadastro de produtos" />
+                            <p className="dsc-menu-item-active">Produtos</p>
+                        </div>
                     </div>
-                    <div className="dsc-logged-user">
-                        <p>Maria Silva</p>
-                        <a href="#">Sair</a>
-                    </div>
+                    <LoggedUser />
                 </div>
             </nav>
         </header>
