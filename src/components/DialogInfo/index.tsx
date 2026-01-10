1ae0ddb3df2b071fbef13e9fe29dafa54a633e94
Gerson Klauck <gk120hf@gmail.com>
2026-01-10 11:03:16 -0300

    Dialog box adjustments

--- src/components/DialogInfo/index.tsx
+++ src/components/DialogInfo/index.tsx
@@ -1,6 +1,5 @@
 
 
-
 import ButtonPrimary from "../ButtonPrimary";
 
 type Props = {
@@ -11,7 +10,7 @@ type Props = {
 export default function DialogInfo({message, onDialogClose}: Props) {
     return(
         <div className="dsc-dialog-background" onClick={() => onDialogClose()}>
-            <div className="dsc-dialog-box" onClick={(event) => event.stopPropagation()}>
+            <div className="dsc-dialog-box dsc-info-box" onClick={(event) => event.stopPropagation()}>
                 <h2>{message}</h2>
                 <div
                     className="dsc-dialog-btn"
